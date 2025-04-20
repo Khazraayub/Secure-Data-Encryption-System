78604c2507bd46a29008bf05d352aa4c27791348
Khazra Ayub <162344197+Khazraayub@users.noreply.github.com>
2025-04-20 08:06:17 -0700

    Secure-Data-Encryption-System.py

--- Secure-Data-Encryption-System.py
+++ Secure-Data-Encryption-System.py
@@ -57,10 +57,10 @@ def encrypt_text(text, key):
 
 
 # DECRYPT TEXT
-def decrypt_text(encrypt_text, key):
+def decrypt_text(encrypted_text, key):
     try:
         cipher = Fernet(generate_key,(key))
-        return cipher.decrypt(encrypt_text.encode()).decode()
+        return cipher.decrypt(encrypted_text.encode()).decode()
     except:
         return None
     
